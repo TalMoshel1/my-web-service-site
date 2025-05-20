41b70ce6f87c1535a83b79e1839483c58011e961
TALMO <talmoshel444@gmail.com>
2025-05-20 20:49:01 +0300

    remove gallery/*

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,5 @@
 import { BlogPostsResponse } from "@/models/BlogPost";
 import { MetadataRoute } from "next";
-import { imagesArray } from "./lib/images";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const response = await fetch("https://dummyjson.com/posts");
@@ -10,20 +9,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${id}`
   }));
 
-  const imagesEntries: MetadataRoute.Sitemap = imagesArray.map((img) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/gallery/${img.id}`,
-  }));
+
 
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
       lastModified: new Date(),
     },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/gallery`,
-      lastModified: new Date(),
-    },
     ...postEntries,
-    ...imagesEntries
   ];
 }
