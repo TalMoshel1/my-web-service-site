35c7cfee9d8e27297b4fc607949f508e556f1b56
TALMO <talmoshel444@gmail.com>
2025-06-16 16:03:10 +0300

    removing import Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import HeroImage from "@/components/HeroImage";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 // import StyledJsxRegistry from "./registry";
 // import Demo1 from "../components/Demo1.svg";
