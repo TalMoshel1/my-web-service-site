63951acefe7a9b98043c039c948766662a56fad9
TALMO <talmoshel444@gmail.com>
2025-07-22 10:21:00 +0300

    styling ul with outer border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import Form from "@/components/Form";
 import FormContainer from "@/components/FormContainer";
 import AnimatedOnScrollButNotOnLoad from "@/components/AnimatedOnScrollButNotOnLoad";
 import "./components-css/AboutIndex.css";
+import AngleTopRightSvg from "@/components/AngleTopRightSvg";
+import AngleBottomLeftSvg from "@/components/AngleBottomLeftSvg";
 
 export const metadata: Metadata = {
   title:
@@ -25,200 +27,222 @@ export default function AboutPage() {
       <AboutHeaderLayout />
 
       <main
-  className={varela.className}
-  style={{
-    position: "relative",
-    top: "0",
-    minHeight: "70svh",
-    height: "fit-content",
-    width: "100vw",
-  }}
->
-  <div
-    className="about-background-container"
-    style={{
-      position: "absolute",
-      top: "0",
-      left: "0",
-      width: "100%",
-      minHeight: "100%",
-      height: "fit-content",
-    }}
-  >
-    <Image
-      src={"/sofa-living-room-with-copy-space2.webp"}
-      alt={"living room"}
-      fill={true}
-    />
-  </div>
-
-  <div
-    style={{
-      position: "relative",
-      top: "0",
-      backgroundColor: "rgba(255,255,255,0.8)",
-      width: "100%",
-      minHeight: "100svh",
-      height: "100%",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <div
-      style={{
-        height: "fit-content",
-        margin: "5.5em auto",
-        width: "auto",
-        // maxWidth: "90%",
-        //           outline: '8px ridge rgba(0, 0, 0,0.2)',
-        // borderRadius: '2rem', backgroundColor:'rgba(79, 159,7,0.3)'
-      }}
-    >
-      <header
+        className={varela.className}
         style={{
-          padding: "2rem",
-          fontSize: "1.5rem",
-          backgroundColor: "transparent",
-          textAlign: "center",
+          position: "relative",
+          top: "0",
+          minHeight: "70svh",
+          height: "fit-content",
+          width: "100vw",
         }}
       >
-        מיקסום הפוטנציאל של האתר שלכם יכול להתבצע במגוון דרכים
-      </header>
-      <AnimatedOnScrollButNotOnLoad threshold={0.1}>
-        <ul
+        <div
+          className="about-background-container"
           style={{
-            width: "80%",
-            margin: "0 auto",
-            gap: "1rem",
-            listStyleType: "none",
-            display: "flex",
-            alignItems: "center",
-            flexWrap: "wrap",
-            paddingTop: "4.5em",
-            paddingBottom: "4.5em",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            minHeight: "100%",
+            height: "fit-content",
           }}
-          className="services"
         >
-          <AnimatedOnScrollButNotOnLoad threshold={0.1}>
-            <li
-              className="service-item"
-              style={{
-                position: "relative",
-                height: "fit-content",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <span style={{}}><a href='/articles/ux-ui'>שיפור חווית משתמש</a></span>
-              {/* <Customer_ux /> */}
-              <Image
-                src={"/customer-ux.svg"}
-                alt={"customer-ux"}
-                width={100}
-                height={100}
-                color="black"
-              />
-            </li>
-          </AnimatedOnScrollButNotOnLoad>
-          <AnimatedOnScrollButNotOnLoad threshold={0.1}>
-            <li
+          <Image
+            src={"/sofa-living-room-with-copy-space2.webp"}
+            alt={"living room"}
+            fill={true}
+          />
+        </div>
 
+        <div
+          style={{
+            position: "relative",
+            top: "0",
+            backgroundColor: "rgba(255,255,255,0.8)",
+            width: "100%",
+            minHeight: "100svh",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "start",
+          }}
+        >
+          <div
             style={{
-              position: "relative",
               height: "fit-content",
+              // margin: "5.5em auto",
+               width: 'calc(90vw - 4rem)',
+              margin: "2rem auto",
               display: "flex",
               flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
+              gap: "1rem",
+              // maxWidth: "90%",
+              //           outline: '8px ridge rgba(0, 0, 0,0.2)',
+              // borderRadius: '2rem', backgroundColor:'rgba(79, 159,7,0.3)'
             }}
-            >
-              <span>בניית עמודי נחיתה שמניעים לפעולה</span>
-              <Image
-                src={"/customer-ux.svg"}
-                alt={"customer-ux"}
-                width={100}
-                height={100}
-              />
-            </li>
-          </AnimatedOnScrollButNotOnLoad>
-
-          <AnimatedOnScrollButNotOnLoad threshold={0.1}>
-            <li
-              className="service-item"
+          >
+            <header
               style={{
-                position: "relative",
-                height: "fit-content",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
+                // padding: "2rem",
+                fontSize: "1.5rem",
+                backgroundColor: "transparent",
+                textAlign: "center",
               }}
             >
-              <span>תיקון באגים</span>
-              {/* <Customer_ux /> */}
-              <Image
-                src={"/customer-ux.svg"}
-                alt={"customer-ux"}
-                width={100}
-                height={100}
-              />
-            </li>
-          </AnimatedOnScrollButNotOnLoad>
+              מיקסום הפוטנציאל של האתר שלכם יכול להתבצע במגוון דרכים
+            </header>
+            <AnimatedOnScrollButNotOnLoad threshold={0.1}>
+              <div className='services-ul-container' style={{ position: "relative", margin: "0 auto" }}>
+                {/* <AngleTopRightSvg /> */}
+                <ul
+                  style={{
+                    width: "100%",
+                    marginTop: "2em",
+                    gap: "1rem",
+                    listStyleType: "none",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "start",
+                    flexWrap: "wrap",
+                    // paddingTop: "4.5em",
+                    // paddingBottom: "4.5em",
+                  }}
+                  className="services"
+                >
+                  <AnimatedOnScrollButNotOnLoad threshold={0.1}>
+                  <div className='fake-border-horizontal' style={{top:'-1rem', right:'-1rem'}}></div>
+                  <div className='fake-border-vertical' style={{top:'-1rem', right:'-1rem'}}></div>
 
-          <AnimatedOnScrollButNotOnLoad threshold={0.1}>
-            <li
-              className="service-item"
-              style={{
-                position: "relative",
-                height: "fit-content",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <span>קידום אורגני</span>
-              <Image
-                src={"/customer-ux.svg"}
-                alt={"customer-ux"}
-                width={100}
-                height={100}
-              />
-            </li>
-          </AnimatedOnScrollButNotOnLoad>
-          <AnimatedOnScrollButNotOnLoad threshold={0.1}>
-            <li
-              className="service-item"
-              style={{
-                position: "relative",
-                height: "fit-content",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <span>שיפור נגישות</span>
-              {/* <Customer_ux /> */}
-              <Image
-                src={"/customer-ux.svg"}
-                alt={"customer-ux"}
-                width={100}
-                height={100}
-              />
-            </li>
-          </AnimatedOnScrollButNotOnLoad>
-        </ul>
-      </AnimatedOnScrollButNotOnLoad>
-    </div>
-  </div>
-</main>
+                  {/* <div style={{width: '5%', height: '30%', position:'absolute', top:'0', right:'-5%', backgroundColor:'red'}}></div> */}
 
-<FormContainer />
+
+                    <li
+                      className="service-item"
+                      style={{
+                        position: "relative",
+                        height: "fit-content",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+
+                      }}
+                    >
+                      <span style={{}}>
+                        <a href="/articles/ux-ui">שיפור חווית משתמש</a>
+                      </span>
+                      {/* <Customer_ux /> */}
+                      <Image
+                        src={"/customer-ux.svg"}
+                        alt={"customer-ux"}
+                        width={100}
+                        height={100}
+                        color="black"
+                      />
+
+                    </li>
+                  </AnimatedOnScrollButNotOnLoad>
+                  <AnimatedOnScrollButNotOnLoad threshold={0.1}>
+                    <li
+                      style={{
+                        position: "relative",
+                        height: "fit-content",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <span>בניית עמודי נחיתה שמניעים לפעולה</span>
+                      <Image
+                        src={"/customer-ux.svg"}
+                        alt={"customer-ux"}
+                        width={100}
+                        height={100}
+                      />
+                    </li>
+                  </AnimatedOnScrollButNotOnLoad>
+
+                  <AnimatedOnScrollButNotOnLoad threshold={0.1}>
+                    <li
+                      className="service-item"
+                      style={{
+                        position: "relative",
+                        height: "fit-content",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <span>תיקון באגים</span>
+                      {/* <Customer_ux /> */}
+                      <Image
+                        src={"/customer-ux.svg"}
+                        alt={"customer-ux"}
+                        width={100}
+                        height={100}
+                      />
+                    </li>
+                  </AnimatedOnScrollButNotOnLoad>
+
+                  <AnimatedOnScrollButNotOnLoad threshold={0.1}>
+                    <li
+                      className="service-item"
+                      style={{
+                        position: "relative",
+                        height: "fit-content",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <span>קידום אורגני</span>
+                      <Image
+                        src={"/customer-ux.svg"}
+                        alt={"customer-ux"}
+                        width={100}
+                        height={100}
+                      />
+                    </li>
+                  </AnimatedOnScrollButNotOnLoad>
+                  <AnimatedOnScrollButNotOnLoad threshold={0.1}>
+                    
+                    
+                    <li
+                      className="service-item"
+                      style={{
+                        position: "relative",
+                        height: "fit-content",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <span>שיפור נגישות</span>
+                      {/* <Customer_ux /> */}
+                      <Image
+                        src={"/customer-ux.svg"}
+                        alt={"customer-ux"}
+                        width={100}
+                        height={100}
+                      />
+
+                    </li>
+                    <div className='fake-border-horizontal' style={{bottom:'-1rem', left:'-1rem'}}></div>
+                    <div className='fake-border-vertical' style={{bottom:'-1rem', left:'-1rem'}}></div>
+                  </AnimatedOnScrollButNotOnLoad>
+                </ul>
+              </div>
+            </AnimatedOnScrollButNotOnLoad>
+          </div>
+        </div>
+      </main>
+
+      <FormContainer />
     </>
   );
 }
