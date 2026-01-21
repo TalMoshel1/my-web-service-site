8b8aebda8d1f4a4876532c6605be0d647af9924e
TALMO <talmoshel444@gmail.com>
2026-01-21 21:40:53 +0200

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,21 +11,31 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/landing-page`,
+      // url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/landing-page`,
+            url: `https://www.talmoshel.co.il/articles/landing-page`,
+
       lastModified: new Date(),
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/accessibility`,
+            url: `https://www.talmoshel.co.il/articles/accessibility`,
+
+      // url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/accessibility`,
       lastModified: new Date(),
     },
         {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/bot`,
+                url: `https://www.talmoshel.co.il/articles/bot`,
+
+      // url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/bot`,
       lastModified: new Date(),
     },    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/bugs`,
+            url: `https://www.talmoshel.co.il/articles/bugs`,
+
+      // url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/bugs`,
       lastModified: new Date(),
     },    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/ux-ui`,
+            url: `https://www.talmoshel.co.il/articles/ux-ui`,
+
+      // url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/ux-ui`,
       lastModified: new Date(),
     },
   ];
